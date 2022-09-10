68e6e69e53564730358cf81e3c419268e81c1e53
Nikhil Sharma <nikhilsharmash65@gmail.com>
2022-09-10 14:09:52 +0530

    Final commit, Final

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 mongoose.connect(
-  `mongodb+srv://nikhil_20:$Ajhar20032001@cluster0.sxabv.mongodb.net/?retryWrites=true&w=majority`
+  `mongodb+srv://nikhil_20:Ajhar20032001@cluster0.sxabv.mongodb.net/?retryWrites=true&w=majority`
 );
 
 app.use(express.urlencoded({ extended: false }));
