630b0d3cce985f4329fe029c7c03795ce2854f98
Nikhil Sharma <nikhilsharmash65@gmail.com>
2022-09-10 13:19:42 +0530

    Added dotenv File

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const mongoose = require(`mongoose`);
 const article = require(`./models/article`);
 const articleRouter = require("./routes/articles");
 const methodOverride = require(`method-override`);
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
 mongoose.connect(
-  "mongodb+srv://nikhil_20:Ajhar20032001@cluster0.sxabv.mongodb.net/?retryWrites=true&w=majority"
+  `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.sxabv.mongodb.net/?retryWrites=true&w=majority`
 );
 
 app.use(express.urlencoded({ extended: false }));
